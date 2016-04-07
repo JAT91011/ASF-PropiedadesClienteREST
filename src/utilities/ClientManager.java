e7e7717b4a0bd9ccc7540a0af06bb7038c9a56c4
Jordan Aranda Tejada <Jordan@MacBook-Pro-de-Jordan.local>
2016-04-08 00:28:47 +0200

    Se añaden pruebas al main (no están revisadas)

--- src/utilities/ClientManager.java
+++ src/utilities/ClientManager.java
@@ -21,7 +21,11 @@ public class ClientManager {
 
 	private static ClientManager	instance;
 
-	private static String			URL	= "http://localhost:8080/PropiedadesServidorREST/";
+	public static int				SUCCESS_CODE		= 200;
+	public static int				ERROR_CODE			= 201;
+	public static int				ALREADY_EXIST_CODE	= 202;
+
+	private static String			URL					= "http://localhost:8080/PropiedadesServidorREST/";
 
 	private DefaultClientConfig		config;
 	private Client					client;
@@ -36,7 +40,7 @@ public class ClientManager {
 	// CLIENTES
 	public Cliente getClienteById(final int id) {
 		ClientResponse response = service.path("rest").path("clientes").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, id);
-		return response.getStatus() == 201 ? response.getEntity(Cliente.class) : null;
+		return response.getStatus() == SUCCESS_CODE ? response.getEntity(Cliente.class) : null;
 	}
 
 	public ArrayList<Cliente> getAllClientes() {
@@ -59,7 +63,7 @@ public class ClientManager {
 	public boolean saveCliente(final Cliente client) {
 		try {
 			ClientResponse response = service.path("rest").path("clientes").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, client);
-			return response.getStatus() == 201 ? true : false;
+			return response.getStatus() == SUCCESS_CODE ? true : false;
 		} catch (UniformInterfaceException e) {
 			ClientResponse r = e.getResponse();
 			System.out.println("todos.{id}.DEL.status: " + r.getStatus());
@@ -72,7 +76,7 @@ public class ClientManager {
 	public boolean saveAlquiler(final Alquiler alquiler) {
 		try {
 			ClientResponse response = service.path("rest").path("alquileres").type(MediaType.APPLICATION_JSON).post(ClientResponse.class, alquiler);
-			return response.getStatus() == 201 ? true : false;
+			return response.getStatus() == SUCCESS_CODE ? true : false;
 		} catch (UniformInterfaceException e) {
 			ClientResponse r = e.getResponse();
 			System.out.println("todos.{id}.DEL.status: " + r.getStatus());
@@ -119,6 +123,59 @@ public class ClientManager {
 	}
 
 	public void main(String[] args) {
-		// TEST
+
+		// INSERTAR CLIENTES
+		System.out.println("INSERTAMOS TRES CLIENTES DE PRUEBA");
+		Cliente cl1 = new Cliente(78952922, "Endika", "Salgueiro", "email@email.com", "Calle falsa 1234", 48012, 123456789);
+		Cliente cl2 = new Cliente(12457845, "Jordan", "Aranda", "email@email.com", "Calle falsa 1234", 48012, 123456789);
+		Cliente cl3 = new Cliente(12345678, "Prueba", "Prueba", "email@email.com", "Calle falsa 1234", 48012, 123456789);
+		if (ClientManager.getInstance().saveCliente(cl1)) {
+			System.out.println("Insertado correctamente");
+		} else {
+			System.out.println("Ya existe");
+		}
+		if (ClientManager.getInstance().saveCliente(cl2)) {
+			System.out.println("Insertado correctamente");
+		} else {
+			System.out.println("Ya existe");
+		}
+		if (ClientManager.getInstance().saveCliente(cl3)) {
+			System.out.println("Insertado correctamente");
+		} else {
+			System.out.println("Ya existe");
+		}
+
+		// OBTENEMOS TODOS LOS CLIENTES DE LA BASE DE DATOS Y LOS MOSTRAMOS
+		System.out.println("\nVISUALIZAMOS TODOS LOS CLIENTES Y SUS ALQUILERES DE LA BASE DE DATOS");
+		ArrayList<Cliente> clientes = ClientManager.getInstance().getAllClientes();
+		for (Cliente c : clientes) {
+			System.out.println(c.toString());
+			for (Alquiler a : c.getAlquileres()) {
+				System.out.println(" - " + a.toString());
+			}
+		}
+
+		// EDITAMOS EL CLIENTE LLAMADO ENDIKA
+		System.out.println("\nEDITAMOS EL CLIENTE 'ENDIKA'");
+		cl1.setNombre("Endika Editado");
+		if (ClientManager.getInstance().saveCliente(cl1)) {
+			System.out.println("Cliente editado.");
+		} else {
+			System.out.println("No se ha podido editar el cliente");
+		}
+
+		// OBTENEMOS EL CLIENTE EDITADO Y LO MOSTRAMOS
+		System.out.println("\nMOSTRAMOS LOS DATOS DEL CLIENTE 'ENDIKA' TRAS LA EDICION");
+		cl1 = ClientManager.getInstance().getClienteById(cl1.getDni());
+		System.out.println(cl1.toString());
+
+		// BORRAMOS EL CLIENTE 3 (EL DE PRUEBA) Y SUS ALQUILERES
+		System.out.println("\nELIMINAMOS EL CLIENTE DE PRUEBA");
+		if (ClientManager.getInstance().deleteCliente(cl3)) {
+			System.out.println("Cliente '" + cl3.getNombre() + "' borrado correctamente de la base de datos.");
+		} else {
+			System.out.println("El cliente '" + cl3.getNombre() + "' no se ha borrado de la base de datos.");
+		}
+
 	}
 }
